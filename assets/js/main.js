752dd829f15b5ed9132a80ca5868e2d094426fd5
jemmiebugs <jemmynat@yahoo.com>
2023-06-09 03:33:18 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 /**
 * Template Name: ZenBlog
 * Updated: Mar 10 2023 with Bootstrap v5.2.3
@@ -197,6 +198,145 @@ document.addEventListener('DOMContentLoaded', () => {
         alert("No more divs"); // alert if there are none left
       }
     });
+  });
+
+
+
+// .modal-subscribe
+function fallbackPjax(options) {
+  var url = $.isFunction(options.url) ? options.url() : options.url,
+      method = options.type ? options.type.toUpperCase() : 'GET'
+
+  var form = $('<form>', {
+    method: method === 'GET' ? 'GET' : 'POST',
+    action: url,
+    style: 'display:none'
+  })
+
+  if (method !== 'GET' && method !== 'POST') {
+    form.append($('<input>', {
+      type: 'hidden',
+      name: '_method',
+      value: method.toLowerCase()
+    }))
+  }
+
+  var data = options.data
+  if (typeof data === 'string') {
+    $.each(data.split('&'), function(index, value) {
+      var pair = value.split('=')
+      form.append($('<input>', {type: 'hidden', name: pair[0], value: pair[1]}))
+    })
+  } else if ($.isArray(data)) {
+    $.each(data, function(index, value) {
+      form.append($('<input>', {type: 'hidden', name: value.name, value: value.value}))
+    })
+  } else if (typeof data === 'object') {
+    var key
+    for (key in data)
+      form.append($('<input>', {type: 'hidden', name: key, value: data[key]}))
+  }
+
+  $(document.body).append(form)
+  form.submit()
+}
+
+  $(document).on('click', '.popup--exit', function(event) {
+    $(".mail__popup").removeClass('active');
+    setTimeout(function() {
+      $(".mail__popup").removeClass('ontop');
+    }, 250);
+    console.log("clicked close btn")
+    localStorage.setItem("popup", "closed")
+    overflowOn();
+    addBinding();
+  });
+
+  $(document).on('click', '.mail', function(event) {
+    // if (screen.width < 500 && window.orientation === 0 && $('body').scrollTop() > 20) {
+    // 	$('html, body').animate({scrollTop: 0}, '250'); 
+    // 	setTimeout(function() {
+    // 		$("#popup--mail").addClass('active ontop');
+    // 	}, 500);
+    // } else if (screen.width < 500 && window.orientation === 0) {
+    // 	$('html, body').animate({scrollTop: 0}, '100'); 
+    // 	setTimeout(function() {
+    // 		$("#popup--mail").addClass('active ontop');
+    // 	}, 250);
+    // } else {
+    // 	$("#popup--mail").addClass('active ontop');
+    // }
+    $("#popup--mail").addClass('active ontop');
+    // overflowOff();
+    removeBinding();
+  });
+
+  // let closeModalBtn = document.querySelector(".jsCloseMail")			
+  // if(closeModalBtn) {
+  // 	closeModalBtn.addEventListener('click', () => {
+  // 		console.log("clicked close btn")
+  // 		localStorage.setItem("popup", "closed")
+  // 	})
+  // };			
+  
+  let getPopup = document.querySelector(".jsModalMail")
+
+  if(localStorage.getItem("popup") === "open" || localStorage.getItem("popup") !== "closed") {
+    console.log("Modal = open")
+    setTimeout(function() {
+      getPopup.classList.add("active","ontop")
+    }, 3000)
+  }
+
+  if(!localStorage.getItem("popup")) { 
+    console.log("Set local storage and its value to open")
+    localStorage.setItem("popup", "open")
+  }
+  
+  // if(localStorage.getItem("popup")) {    
+  // 	if(localStorage.getItem("popup") === "open") {
+  // 		console.log("Modal = open")
+  // 		setTimeout(function() {
+  // 			getPopup.classList.add("active","ontop")
+  // 		}, 3000)
+  // 	}
+  // } else {
+
+  // 	localStorage.setItem("popup", "open")
+  // 	if(localStorage.getItem("popup") !== "closed") {
+  // 		console.log("Modal !== closed")
+  // 		setTimeout(function() {
+  // 			getPopup.classList.add("active","ontop")
+  // 		}, 3000)
+  // 	}
+  // };
+
+  $(document).on('click', '.popup--exit', function(event) {
+    $(".mail__popup").removeClass('active');
+    setTimeout(function() {
+      $(".mail__popup").removeClass('ontop');
+    }, 250);
+    console.log("clicked close btn")
+    localStorage.setItem("popup", "closed")
+    overflowOn();
+    addBinding();
+  });
+
+  $(document).on('submit','#pricingform', function(e){
+    if ($.trim($("#pricingform__email").val()) === "" || $.trim($("#pricingform__firstname").val()) === "" || $.trim($("#pricingform__lastname").val()) === "" || !$("#pricingform__gdpr").is(":checked")) {
+      $("#pricing_signup").addClass("shake");
+      setTimeout(function() {
+        $("#pricing_signup").removeClass("shake");
+      }, 500);
+      return false;
+    }
+    var action = $(this).attr('action');
+    var formdata = $(this).serialize();
+    $('#popup--pricing').addClass("success");
+    $.post(action, formdata, function(data){
+      console.log("success");
+    });
+    return false;
   });
 
   // .modal-backdrop classes
